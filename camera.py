8a4e714a8ca1f3da8a1a39348811df6b7720ecd0
ZhuKerui <kerui.17@intl.zju.edu.cn>
2021-04-30 08:33:42 +0800

    load video ok

--- camera.py
+++ camera.py
@@ -3,9 +3,12 @@ import cv2
 class Camera_Manager:
     def __init__(self):
         self.cap = cv2.VideoCapture()
+        self.file_valid = False
 
     def get_camera_image(self):
-        flag, image = self.cap.read()
+        self.file_valid, image = self.cap.read()
+        if not self.file_valid:
+            return None
         show = cv2.resize(image, (640, 480))
         show = cv2.cvtColor(show, cv2.COLOR_BGR2RGB)
         return show
@@ -15,6 +18,7 @@ class Camera_Manager:
 
     def open(self, camera_number):
         self.cap.open(camera_number)
+        self.file_valid = True
 
     def isOpened(self):
         return self.cap.isOpened()
